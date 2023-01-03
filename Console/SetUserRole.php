b78e670500ed6f75a71911a5458ca1a86292dd5f
rajudiya <rohanajudiya000@gmail.com>
2023-01-03 18:55:06 +0530

    Change User role using Custom Command

---
+++ Console/SetUserRole.php
@@ -0,0 +1,135 @@
+<?php
+/*
+ * Console Class for add Advertisment into DB table.
+ * @category  Salecto
+ * @package   Salecto_Advertisment
+ * @author    Salecto
+ */
+namespace Custom\Command\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Magento\User\Model\UserFactory;
+use Magento\Authorization\Model\RoleFactory;
+
+/**
+ * Class SomeCommand
+ */
+class SetUserRole extends Command
+{
+    /**
+     * command input parameter 'role'
+     */
+    const USERID = 'userid';
+
+    /**
+     * command input parameter 'role'
+     */
+    const ROLEID = 'roleid';
+
+     /**
+     * command input parameter 'email'
+     */
+    const EMAILID = 'emailid';
+
+    /**
+     * User model factory
+     *
+     * @var \Magento\User\Model\UserFactory
+     */
+    protected $_userFactory;
+
+    /**
+     * Factory for user role model
+     *
+     * @var \Magento\Authorization\Model\RoleFactory
+     */
+    protected $_roleFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Salecto\Advertisment\Model\GridModelFactory
+     */
+    public function __construct(
+        UserFactory $userFactory,
+        RoleFactory $roleFactory
+    ) {
+        
+        $this->_userFactory = $userFactory;
+        $this->_roleFactory = $roleFactory;
+        parent::__construct();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this->setName('set:admin');
+        $this->setDescription('It will set user as administrator.');
+        $this->addOption(
+                self::USERID,
+                null,
+                InputOption::VALUE_REQUIRED,
+                'USERID'
+        );
+        $this->addOption(
+                self::ROLEID,
+                null,
+                InputOption::VALUE_REQUIRED,
+                'ROLEID'
+        );
+        $this->addOption(
+                self::EMAILID,
+                null,
+                InputOption::VALUE_REQUIRED,
+                'EMAILID'
+        );
+        parent::configure();
+    }
+
+    /**
+     * Execute the command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return null|int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userId = $input->getOption(self::USERID);
+        $roleId = $input->getOption(self::ROLEID);
+        $emailId = $input->getOption(self::EMAILID);
+        if($userId && $roleId && $emailId){
+            
+            $model = $this->_userFactory->create()->load($userId);
+            $roleName = $this->checkRole($roleId);
+            if ($userId && $model->isObjectNew()) {
+                $output->writeln('<error>User with id = `'.$userId.'`, No longer exisits</error>');
+            } elseif ($roleName === 0) {
+                $output->writeln('<error>User Role with id = `'.$roleId.'`, Not exisits</error>');
+            } else {
+              $model->setUserId($userId);
+              $model->setRoleId($roleId);
+              $model->setEmail($emailId);
+              $model->save();
+              $output->writeln('<info>User with id `'.$userId.'`set as `'.$roleName.'` And Email is `'.$emailId.'` </info>');
+            }
+        }else{
+            $output->writeln('<error>please set both userid and roleid, i.e. --userid=integer --roleid=integer</error>');
+        }
+    }
+
+    private function checkRole($roleId){
+        $role = $this->_roleFactory->create()->load($roleId);
+        if($role->getId()){
+            return $role->getRoleName();
+        } else {
+            return 0;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Command
+# Custom\Command
+- `Custom User Role and assining to user with command line`
+
+## Composer install
+
+- `composer config repositories.Command vcs https://github.com/rajudiya/Command`
+- `composer require custom/command:dev-main`
+
+## Composer uninstall
+
+- `composer remove task/command`
+
+## Preview will be added
+
+- `This is taskt Module`
+- `Images`
+
+## Settings
+- Option `will be updated ..` - no settings
+- `Create a Custom User Role And Add New User From The Magento Backend`
+- Run and Change user Role And Email =>  `php bin/magento set:admin --userid=2 --roleid=3 --email="emailid"`
+
+## Known issues
+
+- **Issues will be here .. Hopfuly not**
+  No issues
+
+## Developer informations
+- Developer Name - Rohan Ajudiya
+- Inkdin url     - https://www.linkedin.com/in/ajudiya-rohan-9a9ab81b3/
+- Contect us     - +91 6353856107
+
+### Install module
+1. Run `php bin/magento setup:upgrade`
+2. Run `php bin/magento setup:di:compile`
+3. Run `php bin/magento s:s:d`
+4. Run `php bin/magento c:c`
+5. run `php bin/magento set:admin --userid=2 --roleid=3 --email="emailid"`
+
+### Uninstall module
+1. Run `php bin/magento setup:di:compile`
+2. Run `php bin/magento s:s:d`
+3. Run `php bin/magento c:c`
+
+### Additional developer notes
+Reference URL `http://mage244.com/`
